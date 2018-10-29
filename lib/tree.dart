388cc1029e81fcb24f051d9c5e25c011a95806e6
Jacob Phillips <jifalops@gmail.com>
2018-10-29 00:55:55 -0400

    going to break things to allow null as a poison pill

--- lib/tree.dart
+++ lib/tree.dart
@@ -1,19 +1,91 @@
 import 'dart:collection';
+import 'dart:math';
 
+/// A tree of arbitrary depth and breadth.
 class Tree<T> {
-  Tree(T rootValue, {this.maxBreadth, this.maxDepth})
-      : root = Node._(rootValue, 1, null) {
+  Tree(T rootValue,
+      {this.maxBreadth,
+      this.maxDepth,
+      int maxNodes,
+      bool startDepthAtOne: false})
+      : assert(maxBreadth == null || maxBreadth >= 2),
+        assert(maxDepth == null || maxDepth >= 1 + (startDepthAtOne ? 1 : 0)),
+        assert(maxNodes == null || maxNodes >= 3),
+        maxNodes =
+            _clampMaxNodes(maxBreadth, maxDepth, maxNodes, startDepthAtOne),
+        root = Node._(rootValue, startDepthAtOne ? 1 : 0, null) {
     root._tree = this;
     _parents[root] = null;
   }
 
+  /// Create a tree by repeatedly calling the [generator] function.
+  ///
+  /// You must specify either [maxNodes] or ([maxDepth] and [maxBreadth]).
+  /// Failing to do so will result an infinite loop.
+  ///
+  /// If [depthFirst] is true and [maxDepth] is specified, depth-first
+  /// generation will be used. If [depthFirst] is false and [maxBreadth] is
+  /// specified, breadth-first generation will be used. Otherwise, generated
+  /// nodes will be attached to the tree by choosing an available parent node at
+  /// random.
+  Tree.generate(T Function(int index) generator,
+      {this.maxBreadth,
+      this.maxDepth,
+      int maxNodes,
+      bool depthFirst,
+      bool startDepthAtOne: false})
+      : assert(maxNodes != null || (maxBreadth != null && maxDepth != null)),
+      assert(maxBreadth == null || maxBreadth >= 2),
+        assert(maxDepth == null || maxDepth >= 1 + (startDepthAtOne ? 1 : 0)),
+        assert(maxNodes == null || maxNodes >= 3),
+        assert(generator != null),
+        maxNodes =
+            _clampMaxNodes(maxBreadth, maxDepth, maxNodes, startDepthAtOne),
+        root = Node._(generator(0), startDepthAtOne ? 1 : 0, null) {
+    root._tree = this;
+    _parents[root] = null;
+
+    final count = this.maxNodes;
+    int index = 1;
+    Node<T> n = root;
+    if (depthFirst == true && maxDepth != null) {
+      while (index < count) {
+        while ((n?.canHaveChildren ?? false) && index < count) {
+          n = n.add(generator(index++));
+        }
+        n = maxBreadth == null ? root : n?.parent ?? root;
+      }
+    } else if (depthFirst == false && maxBreadth != null) {
+      while (index < count && n != null) {
+        while (n.canHaveChildren && index < count) {
+          n.add(generator(index++));
+        }
+        n = n.parent?.childAt(n.position + 1) ?? n.childAt(0);
+      }
+    } else {
+      while (index < count) {
+        do {
+          n = nodes.elementAt(_random.nextInt(nodeCount));
+        } while (!n.canHaveChildren);
+        n.add(generator(index++));
+      }
+    }
+  }
+
   final Node<T> root;
+  final int maxNodes;
+
+  /// The maximum number of child nodes any particular node may have.
   final int maxBreadth;
   final int maxDepth;
   final _parents = HashMap<Node<T>, Node<T>>();
 
+  Random __random;
+  Random get _random => __random ??= Random();
+
   Iterable<Node<T>> get nodes => _parents.keys;
   int get nodeCount => _parents.length;
+  bool get canAddNode => maxNodes == null || nodeCount < maxNodes;
 
   bool contains(Node<T> node) => _parents.containsKey(node);
 
@@ -22,6 +94,17 @@ class Tree<T> {
     node._children.forEach((index, n) {
       _remove(n);
     });
+  }
+
+  /// This can take a very long time if there are few available spots in a large
+  /// tree. If [maxBreadth] or [maxDepth] are null, this will be fast.
+  Node<T> addRandom(T value) {
+    if (!canAddNode) return null;
+    Node n;
+    do {
+      n = nodes.elementAt(_random.nextInt(nodeCount));
+    } while (!n.canHaveChildren);
+    return n.add(value);
   }
 
   /// Returns a list that contains [root] plus the result of calling
@@ -35,8 +118,21 @@ class Tree<T> {
   }
 
   @override
-  String toString() => allNodes(sortByPosition: true).join('\n');
+  String toString({bool depthFirst: false, includePosition: false}) =>
+      allNodes(sortByPosition: true, depthFirst: depthFirst)
+          .map((n) => n.toString(includePosition))
+          .join('\n');
 }
+
+_clampMaxNodes(
+        int maxBreadth, int maxDepth, int maxNodes, bool startDepthAtOne) =>
+    (maxBreadth != null &&
+            maxDepth != null &&
+            (maxNodes == null || maxNodes > maxBreadth * maxDepth))
+        ? ((pow(maxBreadth, maxDepth + (startDepthAtOne ? 0 : 1)) - 1) /
+                (maxBreadth - 1))
+            .truncate()
+        : maxNodes;
 
 class Node<T> {
   Node._(this.value, this._depth, this._position, [this._tree]);
@@ -54,24 +150,31 @@ class Node<T> {
   Node<T> get parent => tree._parents[this];
 
   int get childCount => _children.length;
-  bool get canHaveChildren => tree.maxDepth == null || depth < tree.maxDepth;
-  bool get hasChildren => _children.length > 0;
+  bool get canHaveChildren =>
+      tree.canAddNode &&
+      (tree.maxDepth == null || depth < tree.maxDepth) &&
+      (tree.maxBreadth == null || childCount < tree.maxBreadth);
+
   bool hasChildAt(int position) => _children[position] != null;
   bool canHaveChildAt(int position) =>
       position >= 0 &&
       canHaveChildren &&
       !hasChildAt(position) &&
-      (tree.maxBreadth == null ||
-          (position < tree.maxBreadth && _children.length < tree.maxBreadth));
+      (tree.maxBreadth == null || position < tree.maxBreadth);
 
-  Node<T> add(T value, int position) {
+  /// [position] defaults to [childCount], which gives it list-like behavior.
+  Node<T> add(T value, [int position]) {
+    position ??= childCount;
     if (canHaveChildAt(position)) {
       final node = Node._(value, depth + 1, position, tree);
+      _children[position] = node;
       tree._parents[node] = this;
       return node;
     }
     return null;
   }
+
+  Node<T> childAt(int position) => _children[position];
 
   Node<T> removeChildAt(int position) {
     final node = _children.remove(position);
@@ -125,10 +228,11 @@ class Node<T> {
   }
 
   @override
-  String toString() {
-    final kids = _children.keys.toList()
-      ..sort()
-      ..map((i) => '$i: ${_children[i].value}').join(', ');
+  String toString([bool includePosition = false]) {
+    final keys = _children.keys.toList()..sort();
+    final kids = keys
+        .map((i) => '${includePosition ? '$i: ' : ''}${_children[i].value}')
+        .join(', ');
     return '$value: ($kids)';
   }
 }
--- test/tree_test.dart
+++ test/tree_test.dart
@@ -3,9 +3,14 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final tree = Tree<int>(1);
+    final tree = Tree<int>.generate(20, (i) => i+1, depthFirst: false, maxDepth: 2, maxBreadth: 2);
+    print('\n${tree.maxNodes}');
+  print('\n$tree');
+  print('\n${tree.toString(depthFirst: true)}');
+  tree.root.childAt(0).remove();
+  print('\n${tree.toString(includePosition: true)}');
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(tree.root, isNotNull);
     });
   });
 }
